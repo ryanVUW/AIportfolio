eaa809bfabded5b4f22a43d5d408e131bede71e6
ryanVUW <skeltoryan@myvuw.ac.nz>
2024-04-08 18:09:42 +1200

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Baseline } from "@/components/component/baseline";
+
+export default function Home() {
+  return (
+    
+      <Baseline/>
+
+    
+
+  );
+}
